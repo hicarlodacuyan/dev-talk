237febde3678804599ddc0a7d96b9e815797f07e
Carlo Dacuyan <hi.carlodacuyan@gmail.com>
2023-02-06 20:08:25 +0800

    Delete initial parsers

--- backend/utils/toNewCompanyEntry.ts
+++ backend/utils/toNewCompanyEntry.ts
@@ -1,29 +1,5 @@
 import { CompanyDocument } from '../types/company';
 
-// function isString(text: unknown): text is string {
-//   return typeof text === 'string';
-// }
-
-// function parseContent(content: unknown): string {
-//   if (!content || !isString(content)) {
-//     throw new Error('Incorrect or missing content');
-//   }
-
-//   return content;
-// }
-
-// function isNumber(num: unknown): num is number {
-//   return typeof num === 'number';
-// }
-
-// function parseLikes(likes: unknown): number {
-//   if (!likes || !isNumber(likes)) {
-//     throw new Error('Incorrect or missing value');
-//   }
-
-//   return likes;
-// }
-
 // eslint-disable-next-line @typescript-eslint/no-explicit-any
 function toNewCompanyEntry(obj: any): CompanyDocument {
   const newCompany: CompanyDocument = {
