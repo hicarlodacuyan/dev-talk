39846fa2fa5ee483fbb50daa24d99c27d3f96347
Carlo Dacuyan <hi.carlodacuyan@gmail.com>
2023-02-16 18:49:35 +0800

    Refactor chatRouter & separate controller and routes

---
+++ backend/controllers/chatController.ts
@@ -0,0 +1,66 @@
+import { Request, Response } from "express";
+import jwt from "jsonwebtoken";
+import Chat from "../models/chat";
+import User from "../models/user";
+import Company from "../models/company";
+import getTokenFrom from "../utils/getTokenFrom";
+import config from "../utils/config";
+import toNewChatEntry from "../utils/toNewChatEntry";
+import { ChatDocument } from "../types/chat";
+
+interface JwtPayload {
+  id: string;
+}
+
+const getChats = async (_req: Request, res: Response): Promise<void> => {
+  const chats = await Chat.find({}).populate("user", { username: 1, name: 1 });
+
+  res.json(chats);
+};
+
+const createChat = async (req: Request, res: Response) => {
+  const newChatEntry: ChatDocument = toNewChatEntry(req.body);
+  const token = getTokenFrom(req);
+
+  if (!token) {
+    return res.status(401).json({ error: "token missing" });
+  }
+
+  if (!config.SECRET) {
+    return res.status(500).json({ error: "secret missing" });
+  }
+
+  const decodedToken = jwt.verify(token, config.SECRET) as JwtPayload;
+
+  if (!decodedToken.id) {
+    return res.status(401).json({ error: "token invalid" });
+  }
+
+  const user = await User.findById(decodedToken.id);
+  const company = await Company.findById(newChatEntry.companyId);
+
+  if (!(user && company)) {
+    return res.status(401).json({ error: "Incorrect data" });
+  }
+
+  const chat = new Chat({
+    content: newChatEntry.content,
+    user: user._id,
+    company: company._id,
+  });
+
+  const savedChat = await chat.save();
+
+  user.chats = [...user.chats].concat(savedChat._id);
+  await user.save();
+
+  company.chats = [...company.chats].concat(savedChat._id);
+  await company.save();
+
+  return res.json(savedChat);
+};
+
+export default {
+  getChats,
+  createChat,
+};
--- backend/routes/chatRouter.ts
+++ backend/routes/chatRouter.ts
@@ -1,60 +1,9 @@
 import express from "express";
-import jwt from "jsonwebtoken";
-import Chat from "../models/chat";
-import User from "../models/user";
-import Company from "../models/company";
-import getTokenFrom from "../utils/getTokenFrom";
-import config from "../utils/config";
-
-interface JwtPayload {
-  id: string;
-}
+import chatController from "../controllers/chatController";
 
 const chatsRouter = express.Router();
 
-chatsRouter.get("/", async (_req, res) => {
-  const chats = await Chat.find({}).populate("user", { username: 1, name: 1 });
-
-  res.json(chats);
-});
-
-chatsRouter.post("/", async (req, res) => {
-  const body = req.body;
-  const token = getTokenFrom(req);
-
-  if (!token) {
-    return res.status(401).json({ error: "token missing" });
-  }
-
-  if (!config.SECRET) {
-    return res.status(500).json({ error: "secret missing" });
-  }
-
-  const decodedToken = jwt.verify(token, config.SECRET) as JwtPayload;
-
-  if (!decodedToken.id) {
-    return res.status(401).json({ error: "token invalid" });
-  }
-
-  const user: any = await User.findById(decodedToken.id);
-
-  const company: any = await Company.findById(body.companyId);
-
-  const chat = new Chat({
-    content: body.content,
-    user: user._id,
-    company: company._id,
-  });
-
-  const savedChat = await chat.save();
-
-  user.chats = [...user.chats].concat(savedChat._id);
-  await user.save();
-
-  company.chats = [...company.chats].concat(savedChat._id);
-  await company.save();
-
-  return res.json(savedChat);
-});
+chatsRouter.get("/", chatController.getChats);
+chatsRouter.post("/", chatController.createChat);
 
 export default chatsRouter;
