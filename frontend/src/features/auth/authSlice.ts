852cb2c02bdaddc4a05b411a8683c671478e5066
hicarlodacuyan <hi.carlodacuyan@gmail.com>
2023-02-23 16:09:05 +0800

    Setup authSlice

---
+++ frontend/src/features/auth/authSlice.ts
@@ -0,0 +1,99 @@
+/* eslint-disable @typescript-eslint/no-explicit-any */
+import { createSlice, createAsyncThunk } from "@reduxjs/toolkit";
+import { NewUserCredentials, UserCredentials } from "../../types/user";
+import authService from "../../services/auth";
+
+const loggedInUserTokenJSON = window.localStorage.getItem("loggedInUserToken");
+let user;
+
+if (loggedInUserTokenJSON) {
+  user = JSON.parse(loggedInUserTokenJSON) as UserCredentials;
+}
+
+const initialState = {
+  user: user ? user : undefined,
+  isError: false,
+  isSuccess: false,
+  isLoading: false,
+  message: "",
+};
+
+export const register = createAsyncThunk(
+  "auth/register",
+  async (user: NewUserCredentials, thunkAPI) => {
+    try {
+      return await authService.register(user);
+    } catch (error: any) {
+      return thunkAPI.rejectWithValue(error.response.data);
+    }
+  }
+);
+
+export const login = createAsyncThunk(
+  "auth/login",
+  async (
+    { username, password }: { username: string; password: string },
+    thunkAPI
+  ) => {
+    try {
+      return await authService.login({ username, password });
+    } catch (error: any) {
+      return thunkAPI.rejectWithValue(error.response.data);
+    }
+  }
+);
+
+export const logout = createAsyncThunk("auth/logout", () => {
+  return authService.logout();
+});
+
+export const authSlice = createSlice({
+  name: "auth",
+  initialState,
+  reducers: {
+    reset: (state) => {
+      state.isLoading = false;
+      state.isSuccess = false;
+      state.isError = false;
+      state.message = "";
+    },
+  },
+  extraReducers: (builder) => {
+    builder
+      .addCase(register.pending, (state) => {
+        state.isLoading = true;
+      })
+      .addCase(register.fulfilled, (state, action) => {
+        state.isLoading = false;
+        state.isSuccess = true;
+        state.user = action.payload;
+      })
+      .addCase(register.rejected, (state, action) => {
+        state.isLoading = false;
+        state.isError = true;
+        state.message = action.payload as string;
+        state.user = undefined;
+      })
+      .addCase(login.pending, (state) => {
+        state.isLoading = true;
+      })
+      .addCase(login.fulfilled, (state, action) => {
+        state.isLoading = false;
+        state.isSuccess = true;
+        state.user = action.payload;
+      })
+      .addCase(login.rejected, (state, action) => {
+        state.isLoading = false;
+        state.isError = true;
+        state.message = action.payload as string;
+        state.user = undefined;
+      })
+      .addCase(logout.fulfilled, (state) => {
+        state.user = undefined;
+      });
+  },
+});
+
+export const { reset } = authSlice.actions;
+
+export default authSlice.reducer;
