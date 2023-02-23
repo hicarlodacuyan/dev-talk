ab55a5be3076c373a6c821e5d8a7420f6a3ca9b6
hicarlodacuyan <hi.carlodacuyan@gmail.com>
2023-02-23 16:05:25 +0800

    Setup initial Routes with react-router-dom

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,44 +1,34 @@
-import { useEffect, useState } from "react";
-import LoginForm from "./components/LoginForm";
-import Logout from "./components/Logout";
-import { UserCredentials } from "./types/user";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import ChatRoom from "./pages/ChatRoom";
+import Login from "./pages/Login";
+import Register from "./pages/Register";
 
 const App = () => {
-  const [username, setUsername] = useState("");
-  const [password, setPassword] = useState("");
-  const [user, setUser] = useState<UserCredentials | null>(null);
-
-  useEffect(() => {
-    const loggedInUserTokenJSON =
-      window.localStorage.getItem("loggedInUserToken");
-
-    if (loggedInUserTokenJSON) {
-      const user = JSON.parse(loggedInUserTokenJSON) as UserCredentials;
-      setUser(user);
-    }
-  }, []);
-
-  if (!user) {
-    return (
-      <div className="flex justify-center flex-col h-screen gap-4">
-        <h1 className="text-center text-4xl md:text-6xl font-bold text-slate-300">
-          <span className="text-slate-500 font-extrabold">Dev</span>Talk
-        </h1>
-        <LoginForm
-          username={username}
-          setUsername={setUsername}
-          password={password}
-          setPassword={setPassword}
-          setUser={setUser}
-        />
-      </div>
-    );
-  }
+  // if (!user) {
+  //   return (
+  //     <div className="flex justify-center flex-col h-screen gap-4">
+  //       <h1 className="text-center text-4xl md:text-6xl font-bold text-slate-300">
+  //         <span className="text-slate-500 font-extrabold">Dev</span>Talk
+  //       </h1>
+  //       <LoginForm
+  //         username={username}
+  //         setUsername={setUsername}
+  //         password={password}
+  //         setPassword={setPassword}
+  //         setUser={setUser}
+  //       />
+  //     </div>
+  //   );
+  // }
 
   return (
-    <div>
-      <Logout setUser={setUser} />
-    </div>
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<ChatRoom />} />
+        <Route path="/login" element={<Login />} />
+        <Route path="/register" element={<Register />} />
+      </Routes>
+    </BrowserRouter>
   );
 };
 
