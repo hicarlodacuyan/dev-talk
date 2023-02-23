35ea7e002de359e518fc2a470ee16e73a81fdcc9
hicarlodacuyan <hi.carlodacuyan@gmail.com>
2023-02-23 16:10:02 +0800

    Add auth service

---
+++ frontend/src/services/auth.ts
@@ -0,0 +1,39 @@
+import axios, { AxiosResponse } from "axios";
+import { NewUserCredentials, UserCredentials } from "../types/user";
+
+const login = async ({
+  username,
+  password,
+}: {
+  username: string;
+  password: string;
+}) => {
+  const response: AxiosResponse<UserCredentials> = await axios.post(
+    "api/login",
+    {
+      username,
+      password,
+    }
+  );
+  localStorage.setItem("loggedInUserToken", JSON.stringify(response.data));
+  return response.data;
+};
+
+const register = async (newUserCredential: NewUserCredentials) => {
+  const response: AxiosResponse<NewUserCredentials> = await axios.post(
+    "/api/users",
+    newUserCredential
+  );
+
+  return response.data;
+};
+
+const logout = () => {
+  localStorage.removeItem("loggedInUserToken");
+};
+
+export default {
+  login,
+  register,
+  logout,
+};
