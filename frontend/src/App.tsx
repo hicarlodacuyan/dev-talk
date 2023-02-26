f4a54f8f5c155788d83f5da191d357b995e6b03c
Carlo Dacuyan <hi.carlodacuyan@gmail.com>
2023-02-26 10:01:17 +0800

    Delete unneeded comment in App component

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -4,23 +4,6 @@ import Login from "./pages/Login";
 import Register from "./pages/Register";
 
 const App = () => {
-  // if (!user) {
-  //   return (
-  //     <div className="flex justify-center flex-col h-screen gap-4">
-  //       <h1 className="text-center text-4xl md:text-6xl font-bold text-slate-300">
-  //         <span className="text-slate-500 font-extrabold">Dev</span>Talk
-  //       </h1>
-  //       <LoginForm
-  //         username={username}
-  //         setUsername={setUsername}
-  //         password={password}
-  //         setPassword={setPassword}
-  //         setUser={setUser}
-  //       />
-  //     </div>
-  //   );
-  // }
-
   return (
     <BrowserRouter>
       <Routes>
