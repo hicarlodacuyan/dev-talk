e32f0e932fd32eb18faa00e0ca5ed43e542e1e4a
Carlo Dacuyan <hi.carlodacuyan@gmail.com>
2023-02-26 08:51:34 +0800

    Prevent user from accessing ChatRoom if not login

--- frontend/src/pages/ChatRoom.tsx
+++ frontend/src/pages/ChatRoom.tsx
@@ -1,6 +1,15 @@
-import React from "react";
+import { useEffect } from "react";
+import { useNavigate } from "react-router-dom";
+import { useAppSelector } from "../app/hooks";
 
 const ChatRoom = () => {
+  const navigate = useNavigate();
+  const { user } = useAppSelector((state) => state.auth);
+
+  useEffect(() => {
+    if (!user) navigate("/login");
+  }, [user, navigate]);
+
   return <div>ChatRoom</div>;
 };
 
