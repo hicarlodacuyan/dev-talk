6d4af2ca34e99138a91c99e5b0864504ef07c90b
Carlo Dacuyan <hi.carlodacuyan@gmail.com>
2023-02-26 09:29:21 +0800

    Add check if there is not a user token, navigate back to login

--- frontend/src/pages/ChatRoom.tsx
+++ frontend/src/pages/ChatRoom.tsx
@@ -1,16 +1,22 @@
 import { useEffect } from "react";
 import { useNavigate } from "react-router-dom";
 import { useAppSelector } from "../app/hooks";
+import Logout from "../components/Logout";
 
 const ChatRoom = () => {
   const navigate = useNavigate();
   const { user } = useAppSelector((state) => state.auth);
 
   useEffect(() => {
-    if (!user) navigate("/login");
+    if (!user?.token) navigate("/login");
   }, [user, navigate]);
 
-  return <div>ChatRoom</div>;
+  return (
+    <div>
+      {`You are logged in as ${user?.name}`}
+      <Logout />
+    </div>
+  );
 };
 
 export default ChatRoom;
